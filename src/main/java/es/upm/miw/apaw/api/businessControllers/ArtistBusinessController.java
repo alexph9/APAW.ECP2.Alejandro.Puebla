51f04d34332ea235323979d21b40394cb2b34c8e
alexph9 <alexph9@hotmail.com>
2018-10-08 23:52:09 +0200

    Create updateName in ArtistBusinessController and NotFoundException #02Â

--- src/main/java/es/upm/miw/apaw/api/businessControllers/ArtistBusinessController.java
+++ src/main/java/es/upm/miw/apaw/api/businessControllers/ArtistBusinessController.java
@@ -3,6 +3,7 @@ package es.upm.miw.apaw.api.businessControllers;
 import es.upm.miw.apaw.api.daos.DaoFactory;
 import es.upm.miw.apaw.api.dtos.ArtistDto;
 import es.upm.miw.apaw.api.entities.Artist;
+import es.upm.miw.apaw.api.exceptions.NotFoundException;
 
 public class ArtistBusinessController {
 
@@ -10,5 +11,11 @@ public class ArtistBusinessController {
         Artist artist = new Artist(artistDto.getName(), true);
         DaoFactory.getFactory().getArtistDao().save(artist);
         return artist.getId();
+    }
+
+    public void updateName(String id, ArtistDto artistDto){
+        Artist artist = DaoFactory.getFactory().getArtistDao().read(id).orElseThrow(() -> new NotFoundException("Artist id: " + id));
+        artist.setName(artistDto.getName());
+        DaoFactory.getFactory().getArtistDao().save(artist);
     }
 }
