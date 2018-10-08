8c188b7edef4715419eaeb1150d1bc044e9408d5
alexph9 <alexph9@hotmail.com>
2018-10-08 22:37:39 +0200

    Create ArtistRestController and ArgumentNotValidException #01Â

---
+++ src/main/java/es/upm/miw/apaw/api/restControllers/ArtistRestController.java
@@ -0,0 +1,24 @@
+package es.upm.miw.apaw.api.restControllers;
+
+import es.upm.miw.apaw.api.businessControllers.ArtistBusinessController.ArtistBusinessController;
+import es.upm.miw.apaw.api.dtos.ArtistDto;
+import es.upm.miw.apaw.api.exceptions.ArgumentNotValidException;
+
+public class ArtistRestController {
+
+    public static final String ARTISTS = "/artists";
+
+    private ArtistBusinessController artistBusinessController = new ArtistBusinessController();
+
+    private void validate(Object property, String message){
+        if(property == null){
+            throw new ArgumentNotValidException(message + " is NULL");
+        }
+    }
+
+    public String create(ArtistDto artistDto){
+        this.validate(artistDto, "artistDto");
+        this.validate(artistDto.getName(), "ArtistDto name");
+        return this.artistBusinessController.create(artistDto);
+    }
+}
