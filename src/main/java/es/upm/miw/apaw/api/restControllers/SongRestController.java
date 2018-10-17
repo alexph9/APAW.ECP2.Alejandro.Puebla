a7aae8fdb26b68b4c1bea129c5153db4d2ce4777
alexph9 <alexph9@hotmail.com>
2018-10-18 00:06:18 +0200

    Create SongRestController and change Dispatcher #04

---
+++ src/main/java/es/upm/miw/apaw/api/restControllers/SongRestController.java
@@ -0,0 +1,27 @@
+package es.upm.miw.apaw.api.restControllers;
+
+import es.upm.miw.apaw.api.businessControllers.SongBusinessController;
+import es.upm.miw.apaw.api.dtos.SongDto;
+import es.upm.miw.apaw.api.exceptions.ArgumentNotValidException;
+
+public class SongRestController {
+
+    public static final String SONGS = "/songs";
+
+    private SongBusinessController songBusinessController = new SongBusinessController();
+
+    public String create(SongDto songDto){
+        this.validate(songDto, "songDto");
+        this.validate(songDto.getName(), "songDto name");
+        this.validate(songDto.getGenre(), "songDto genre");
+        this.validate(songDto.getArtistId(), "songDto artist id");
+        return songBusinessController.create(songDto);
+
+    }
+
+    private void validate(Object property, String message) {
+        if (property == null) {
+            throw new ArgumentNotValidException(message + " is NULL");
+        }
+    }
+}
