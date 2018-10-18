dd3de433348d1bc4125b49199c64183bec7e9de2
alexph9 <alexph9@hotmail.com>
2018-10-18 22:35:07 +0200

    Create tests and refactoring #06

--- src/test/java/es/upm/miw/apaw/api/SongIT.java
+++ src/test/java/es/upm/miw/apaw/api/SongIT.java
@@ -4,6 +4,7 @@ import es.upm.miw.apaw.api.daos.DaoFactory;
 import es.upm.miw.apaw.api.daos.memory.DaoMemoryFactory;
 import es.upm.miw.apaw.api.dtos.ArtistDto;
 import es.upm.miw.apaw.api.dtos.SongDto;
+import es.upm.miw.apaw.api.dtos.SongIdNameDto;
 import es.upm.miw.apaw.api.entities.Genre;
 import es.upm.miw.apaw.api.restControllers.ArtistRestController;
 import es.upm.miw.apaw.api.restControllers.SongRestController;
@@ -14,8 +15,11 @@ import es.upm.miw.apaw.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SongIT {
 
@@ -24,16 +28,20 @@ public class SongIT {
         DaoFactory.setFactory(new DaoMemoryFactory());
     }
 
+    @Test
+    void testCreateSong(){
+        this.createSong("Song one");
+    }
+
     private String createArtist(){
         HttpRequest request = HttpRequest.builder(ArtistRestController.ARTISTS).body(new ArtistDto("U2")).post();
         return (String) new Client().submit(request).getBody();
     }
 
-    @Test
-    void createSong(){
+    private void createSong(String song){
         String artistId = this.createArtist();
         HttpRequest request = HttpRequest.builder(SongRestController.SONGS)
-                .body(new SongDto("Revolutions", artistId, Genre.ROCK)).post();
+                .body(new SongDto(song, artistId, Genre.ROCK)).post();
         new Client().submit(request);
     }
 
@@ -52,6 +60,16 @@ public class SongIT {
                 .body(new SongDto("Revolution",  artistId, null)).post();
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testReadAll() {
+        for (int i = 0; i < 5; i++) {
+            this.createSong("song"+i);
+        }
+        HttpRequest request = HttpRequest.builder(SongRestController.SONGS).get();
+        List<SongIdNameDto> songs = (List<SongIdNameDto>) new Client().submit(request).getBody();
+        assertTrue(songs.size()>=5);
     }
 
 }
